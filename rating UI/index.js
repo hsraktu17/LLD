13f2351f42856380ef7f66f80c4aa823e3a83b8c
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-05-07 09:50:04 +0530

    made changes

--- rating UI/index.js
+++ rating UI/index.js
@@ -1,32 +1,27 @@
-const starsContainer = document.getElementById('stars');
-const ratingText = document.getElementById('ratingText');
-let selectedRating = 0;
+const starContainer = document.getElementById('stars')
+const ratingText = document.getElementById('ratingText')
+let starRating = 0
 
-function renderStars(hoveredRating = 0) {
-  starsContainer.innerHTML = ''; // clear current stars
-
-  for (let i = 1; i <= 5; i++) {
+function renderStar(rating = 0) {
+  starContainer.innerHTML = ''
+  for(let i = 1; i <= 5; i++) {
     const star = document.createElement('span');
     star.classList.add('star');
     star.innerText = '★';
-
-    // fill if hovered or selected
-    if (i <= hoveredRating || i <= selectedRating) {
-      star.classList.add('filled');
+    if(i <= rating || i <= starRating) {
+      star.classList.add('filled')
     }
-
-    // mouse interactions
-    star.addEventListener('mouseover', () => renderStars(i));
-    star.addEventListener('mouseout', () => renderStars());
+      
+    
+    star.addEventListener('mouseover', () => renderStar(i))
+    star.addEventListener('mouseout', ()=> renderStar())
     star.addEventListener('click', () => {
-      selectedRating = i;
-      console.log(selectedRating)
-      ratingText.innerText = `You rated: ${selectedRating} star${selectedRating > 1 ? 's' : ''}`;
-    });
+      selectedRating = i
+      ratingText.innerHTML = `your rated ${starRating} star${starRating > 1 ? 's' : ''}`
+    })
 
-    starsContainer.appendChild(star);
+    starContainer.appendChild(star)
   }
 }
 
-// initial render
-renderStars();
+renderStar()
